27543e2c95734216ee925534bb520480fedf66b6
William Hefter <william@whefter.de>
2015-06-16 23:28:28 +0200

    Set source branch to master

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,7 +1,7 @@
 class rotating_rsync_backup (
   $ensure               = 'present',
   $source_repo          = 'https://github.com/whefter/rotating-rsync-backup.git',
-  $source_branch        = undef,
+  $source_branch        = 'master',
   $source_ensure        = 'present',
   $installpath          = '/usr/share/rotating-rsync-backup',
   $configpath           = '/etc/rotating-rsync-backup',
